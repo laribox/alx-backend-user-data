ad2ea4403bc79e4bc472a17566cc048755a6dde0
laribox <laribox2014@gmail.com>
2024-11-07 18:47:46 +0000

    task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -22,3 +22,24 @@ def filter_datum(fields: List[str], redaction: str,
     """
     pattern, repl = regex["pattern"], regex["repl"]
     return re.sub(pattern(fields, separator), repl(redaction), message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ filter values in incoming log records using filter_datum """
+
+        if not isinstance(record, logging.LogRecord):
+            raise TypeError("record must be an instance of logging.LogRecord")
+        msg = super(RedactingFormatter, self).format(record)
+        return filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)

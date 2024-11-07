2dced1a51d3f6a825439912b9ca84f6fff882c52
laribox <laribox2014@gmail.com>
2024-11-07 21:30:20 +0000

    task 6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -19,3 +19,16 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validates if a password matches the hashed password.
+
+    Args:
+        hashed_password (bytes): The hashed password.
+        password (str): The plain text password to verify.
+
+    Returns:
+        bool: True if the password matches the hash, False otherwise.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)

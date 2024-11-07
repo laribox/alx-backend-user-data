3a0a21b30edac4a0789613935f749f058fc20afa
laribox <laribox2014@gmail.com>
2024-11-07 18:56:53 +0000

    task 2

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -14,6 +14,7 @@ regex = {
     "pattern": lambda f, s: r"(?P<field>{})=[^{}]+".format("|".join(f), s),
     "repl": lambda r: r"\g<field>={}".format(r),
 }
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -43,3 +44,23 @@ class RedactingFormatter(logging.Formatter):
             raise TypeError("record must be an instance of logging.LogRecord")
         msg = super(RedactingFormatter, self).format(record)
         return filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
+
+
+def get_logger() -> logging.Logger:
+    """
+    Creates and configures a logger for
+    handling user data with PII obfuscation.
+
+    Returns:
+        logging.Logger: A configured logger with RedactingFormatter.
+    """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
+
+    logger.addHandler(stream_handler)
+
+    return logger

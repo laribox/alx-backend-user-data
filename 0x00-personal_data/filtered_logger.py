05cd5c0eebfc68756c39bbd7dae6eae53c6e6278
laribox <laribox2014@gmail.com>
2024-11-07 20:44:29 +0000

    task 3

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -64,3 +64,53 @@ def get_logger() -> logging.Logger:
     logger.addHandler(stream_handler)
 
     return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """
+    connect to mysql database
+    """
+    db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = os.getenv("PERSONAL_DATA_DB_NAME", "my_db")
+    db_user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    db_pwd = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+
+    try:
+        connector = mysql.connector.connect(
+            host=db_host,
+            user=db_user,
+            password=db_pwd,
+            database=db_name,
+        )
+    except mysql.connector.Error:
+        return None
+
+    return connector
+
+
+def main() -> None:
+    """
+    function will obtain a database connection using get_db and retrieve
+    all rows in the users table and display each row under a filtered format
+    """
+    db = get_db()
+    if not db:
+        return
+
+    logger = get_logger()
+    cursor = db.cursor()
+    columns = "name,email,phone,ssn,password,ip,last_login,user_agent"
+    cursor.execute("SELECT {} FROM users".format(columns))
+    rows = cursor.fetchall()
+    fmt = "name={}; email={}; phone={}; ssn={}; password={}; ip={};" + \
+        " last_login={}; user_agent={};"
+
+    for row in rows:
+        formatted_row = fmt.format(
+            row[0], row[1], row[2], row[3], row[4],
+            row[5], row[6], row[7]
+        )
+        logger.info(formatted_row)
+
+    cursor.close()
+    db.close()

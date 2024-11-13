93fbc71a6d8e16f7c1545e02854d5a04d9215c68
laribox <laribox2014@gmail.com>
2024-11-13 19:33:33 +0000

    task 1

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Module for authentication using Session auth
+"""
+
+
+from .auth import Auth
+
+from models.user import User
+from uuid import uuid4
+
+
+class SessionAuth(Auth):
+    """_summary_
+    """
+    pass

e567ac4534dc113a1ebc6940894118d6e9262eaf
laribox <laribox2014@gmail.com>
2024-11-21 21:15:39 +0000

    fix task 5

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,6 +5,7 @@ Auth module for user authentication.
 import bcrypt
 from db import DB
 from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:

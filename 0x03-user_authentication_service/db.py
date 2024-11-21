4fcb180fad56e92ce47e5d8187a5da2b797e5835
laribox <laribox2014@gmail.com>
2024-11-21 21:08:00 +0000

    readme content

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -29,10 +29,14 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """Adds a user to the database."""
-        user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
-        self._session.commit()
-        return user
+        try:
+            new_user = User(email=email, hashed_password=hashed_password)
+            self._session.add(new_user)
+            self._session.commit()
+        except Exception:
+            self._session.rollback()
+            new_user = None
+        return new_user
 
     def find_user_by(self, **kwargs) -> User:
         """Finds a user by attributes."""

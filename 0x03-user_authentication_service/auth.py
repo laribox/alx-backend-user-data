29b2792a9ba0b9dec868fd5af136e2799c6ffa23
laribox <laribox2014@gmail.com>
2024-11-22 00:31:19 +0000

    task 8

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -35,10 +35,15 @@ class Auth:
 
     def valid_login(self, email: str, password: str) -> bool:
         """Validate a user's login credentials."""
-        user = self._db.find_user_by(email=email)
-        if user and bcrypt.checkpw(password.encode('utf-8'),
-                                   user.hashed_password):
-            return True
+        try:
+            user = self._db.find_user_by(email=email)
+            if user is not None:
+                return bcrypt.checkpw(
+                    password.encode("utf-8"),
+                    user.hashed_password,
+                )
+        except NoResultFound:
+            return False
         return False
 
     def create_session(self, email: str) -> str:

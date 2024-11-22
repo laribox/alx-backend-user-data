85de6bfe7dbe0f3d2fbc30186bd6154cdaa975bb
laribox <laribox2014@gmail.com>
2024-11-22 01:00:16 +0000

    task 10

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -25,7 +25,7 @@ def users():
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
-    
+
 
 @app.route('/sessions', methods=['POST'], strict_slashes=False)
 def login() -> str:

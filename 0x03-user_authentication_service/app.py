1d3d4f6fd760b4a754667fecce2c9a0581484071
laribox <laribox2014@gmail.com>
2024-11-22 00:57:12 +0000

    task 10

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -39,5 +39,16 @@ def login() -> str:
     response.set_cookie("session_id", session_id)
     return response
 
+@app.route('/sessions', methods=['DELETE'])
+def logout():
+    """Log out a user."""
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if not user:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect('/')
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
